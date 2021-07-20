8e852ffb8e675e1ab244ed4521ca6c25a73a6601
Mariia Udalova <ASCHA222011@gmail.com>
2021-07-20 10:08:03 +0300

    for compliling

--- src/components/Filters/SizeItem.js
+++ src/components/Filters/SizeItem.js
@@ -3,12 +3,6 @@
 import React from "react";
 
 const SizeItem = ({ value }) => {
-  
-  activeValue={colorsList[0]}
-  listOfValues={colorsList}
-  changeFilterHandler={changeColorHandler}
-
-
   return <li>{value}</li>;
 };
 
